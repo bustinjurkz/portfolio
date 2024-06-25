87a5278096cf11341e531b16fdfc5535a522083d
bustinjurkz <dustinjurkaulionis@gmail.com>
2024-06-26 00:57:06 +1200

    more padding

--- styles/ContentsStyle.ts
+++ styles/ContentsStyle.ts
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 export const ContentsWrapper = styled.div``;
 
 export const InnerWrapper = styled.div`
-  padding: 0 2rem;
+  padding: 0 2.5rem;
   max-width: 1100px;
   margin: auto;
 
