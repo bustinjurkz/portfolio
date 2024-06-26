1d56c75982604b4610d6577d6f2f2ab0e893e4e0
bustinjurkz <dustinjurkaulionis@gmail.com>
2024-06-26 23:41:03 +1200

    header responsive

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -7,17 +7,9 @@ export const SectionHeader = ({ headerText }: SectionHeaderProps) => {
   return (
     <SectionHeaderWrapper>
       <SectionHeaderText>{headerText}</SectionHeaderText>
-
-      <RightSidePattern />
     </SectionHeaderWrapper>
   );
 };
-
-const RightSidePattern = styled.div`
-  width: 57%;
-  background-color: #dadada;
-  height: 55px;
-`;
 
 const SectionHeaderWrapper = styled.div`
   align-items: center;
@@ -26,7 +18,7 @@ const SectionHeaderWrapper = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
-  height: 55px;
+  height: 45px;
 `;
 
 const SectionHeaderText = styled.h2`
@@ -36,7 +28,12 @@ const SectionHeaderText = styled.h2`
   align-content: center;
   z-index: 1;
   height: inherit;
-  width: 43%;
+  width: 100%;
   text-align: center;
   margin-right: 2px;
+  font-size: 1.125rem;
+
+  @media screen and (min-width: 768px) {
+    font-size: 1.5rem;
+  }
 `;
